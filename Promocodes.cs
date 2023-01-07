4bd7d53f9f04809e22a4b65822c5b03570ebecd9
Nova <69091361+qzxtu@users.noreply.github.com>
2023-01-07 05:43:33 -0500

    Add files via upload

---
+++ Promocodes.cs
@@ -0,0 +1,22 @@
+﻿using HtmlAgilityPack;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var html = @"https://genshin-impact.fandom.com/wiki/Promotional_Code";
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument htmlDoc = web.Load(html);
+            var nodesu2 = from nodeuSE in htmlDoc.DocumentNode.Descendants("tr")
+                          select nodeuSE;
+
+            foreach (var tr in htmlDoc.DocumentNode.Descendants().Where(x => x.Name == "tr"))
+            {
+                foreach (var td in tr.Descendants().Where(x => x.Name == "code"))
+                    Console.WriteLine(td.InnerText.Trim());
+            }
+        }
+    }
+}
